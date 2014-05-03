7e2d5dd580b00bccec71e6fd3cc4a34e002b9cdf
straup <straup@gmail.com>
2014-05-03 15:02:51 +0200

    highlight pages

---
+++ www/user_highlights.php
@@ -0,0 +1,28 @@
+<?php
+
+	include("include/init.php");
+	login_ensure_loggedin();
+
+	$viewer = $GLOBALS['cfg']['user'];
+	$owner = $viewer;
+
+	$more = array();
+
+	if ($page = get_int32("page")){
+		$more['page'] = $page;
+	}
+
+	$rsp = dogeared_highlights_for_user($owner, $more);
+	$highlights = array();
+
+	foreach ($rsp['rows'] as $row){
+		$highlights[] = $row;
+	}
+
+	$GLOBALS['smarty']->assign_by_ref("highlights", $highlights);
+	$GLOBALS['smarty']->assign_by_ref("pagination", $rsp['pagination']);
+	
+	$GLOBALS['smarty']->display("page_user_highlights.txt");
+	exit();
+
+?>
