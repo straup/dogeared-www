5d9ea3ba7a4070048c01738bed4a05175c73ef71
straup <straup@gmail.com>
2014-05-03 06:49:56 +0200

    super primitive highlighting controls

--- www/javascript/dogeared.document.js
+++ www/javascript/dogeared.document.js
@@ -5,7 +5,9 @@ function dogeared_document_init(){
     $(document).mouseup(dogeared_document_onselected);
 }
 
-function dogeared_document_onselected(){
+function dogeared_document_onselected(e){
+
+    console.log(e);
 
     var s = dogeared_document_selected();
 
@@ -13,11 +15,39 @@ function dogeared_document_onselected(){
 	return;
     }
 
-    console.log(s);
+    // console.log(s);
 };
 
 function dogeared_document_selected(){
 
-    var s = window.getSelection();
-    return (s) ? s.toString() : null;
+    // http://stackoverflow.com/questions/3597116/insert-html-after-a-selection
+
+    sel = window.getSelection();
+
+    range = window.getSelection().getRangeAt(0);
+    expandedSelRange = range.cloneRange();
+    range.collapse(false);
+
+    // Range.createContextualFragment() would be useful here but is
+    // non-standard and not supported in all browsers (IE9, for one)
+
+    var el = document.createElement("div");
+    el.innerHTML = " POO ";
+
+    var frag = document.createDocumentFragment(), node, lastNode;
+
+    while ((node = el.firstChild)){
+        lastNode = frag.appendChild(node);
+    }
+    
+    range.insertNode(frag);
+
+    // Preserve the selection
+
+    if (lastNode){
+        expandedSelRange.setEndAfter(lastNode.previousSibling);
+        sel.removeAllRanges();
+        sel.addRange(expandedSelRange);
+    }
+
 }
