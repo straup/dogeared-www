e9bf04070b070eddad54e51487822f098b5e664d
straup <straup@gmail.com>
2014-05-05 14:34:04 +0200

    JS all the things

--- www/javascript/dogeared.feedback.js
+++ www/javascript/dogeared.feedback.js
@@ -1,4 +1,17 @@
+var de_feedback_timeout = null;
+
 function dogeared_feedback(msg){
+
     var b = $("#feedback-general");
     b.html(msg);
+
+    if (de_feedback_timeout){
+	clearTimeout(de_feedback_timeout);
+    }
+
+    de_feedback_timeout = setTimeout(function(){
+	var b = $("#feedback-general");
+	b.html("");
+    }, 10000);
+
 }
--- www/javascript/dogeared.js
+++ www/javascript/dogeared.js
@@ -1,3 +1,19 @@
+function dogeared_init(){
+
+    $("#dogeared-navi li a").click(function(){
+
+	var el = $(this);
+
+	if (! dogeared_network_is_online()){
+	    dogeared_feedback("The internet says NO.");
+	    return false;
+	}
+    });
+    
+    dogeared_network_init();
+    dogeared_cache_highlights_init();
+}
+
 function dogeared_abs_root_url(){
 	return document.body.getAttribute("data-abs-root-url");
 }
