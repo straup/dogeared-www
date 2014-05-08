1db6e638f6e33f10fdacd9604c98cf30e1372dae
straup <straup@gmail.com>
2014-05-08 04:56:08 +0200

    delete highlights

--- www/javascript/dogeared.highlights.js
+++ www/javascript/dogeared.highlights.js
@@ -1,5 +1,9 @@
 function dogeared_highlights_init(){
 
+}
+
+function dogeared_highlights_init_list(){
+
     $(".delete-highlight").click(function(){
 
 	if (! confirm("Are you sure you want to delete this highlight?")){
@@ -13,11 +17,35 @@ function dogeared_highlights_init(){
 	var args = { 'id': id };
 
 	var on_success = function(rsp){
-	    console.log(rsp);
+	    var hl = $("#highlight-" + id);
+	    hl.remove();
+	    dogeared_feedback("That highlight has been deleted.");
 	};
 
-	console.log(args);
+	dogeared_api_call(method, args, on_success);
+    });
+}
 
-	dogeared_api_call(method, args);
+function dogeared_highlights_init_single(){
+
+    $(".delete-highlight").click(function(){
+
+	if (! confirm("Are you sure you want to delete this highlight?")){
+	    return false;
+	}
+
+	var el = $(this);
+	var id = el.attr('data-highlight-id');
+
+	var method = "dogeared.highlights.deleteHighlight";
+	var args = { 'id': id };
+
+	var on_success = function(rsp){
+	    var root = dogeared_abs_root_url();
+	    var redir = root + "highlights/?deleted=1";
+	    location.href = redir;
+	};
+	
+	dogeared_api_call(method, args, on_success);
     });
 }
