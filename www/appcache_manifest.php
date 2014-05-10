e53f47bd65003527df627d9902785fb5fcec0dd6
straup <straup@gmail.com>
2014-05-10 17:13:11 +0200

    appcache manifest versioning

--- www/appcache_manifest.php
+++ www/appcache_manifest.php
@@ -7,7 +7,14 @@
 
 	header("Content-Type: text/cache-manifest");
 
-	$version = time();
+	$version = $GLOBALS['cfg']['appcache_manifest_version'];
+
+	if ((! $version) || ($version == 'stat')){
+		$path = $GLOBALS['smarty']->template_dir . "/page_appcache_manifest.txt";
+		$info = stat($path);
+		$version = $info[9];
+	}
+
 	$GLOBALS['smarty']->assign("manifest_version", $version);
 
 	$GLOBALS['smarty']->display("page_appcache_manifest.txt");
