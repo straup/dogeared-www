77abc8f63429003cb14398813c057a2f475ab3dd
straup <straup@gmail.com>
2014-06-01 16:44:43 +0200

    remove comments

--- www/include/lib_dogeared_extruder.php
+++ www/include/lib_dogeared_extruder.php
@@ -69,10 +69,6 @@
 
 		$service = urlencode($service);
 
-		# FIX ME (20140601/straup)
-		# curl_setopt(): The usage of the @filename API for file uploading is deprecated.
-		# Please use the CURLFile class instead
-
 		$args = array();
 
 		if (function_exists("curl_file_create")){
